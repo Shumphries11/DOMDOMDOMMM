1764403287298ad92dd0625953029218581abb9a
shumphries11 <santitahumphries@gmail.com>
2022-02-11 11:58:56 -0600

    Done

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let divContainer = document.createElement('div');
 
     divContainer.className = "container";
 
-    document.body.appendChild(divContainer);
+    
 
     let button = document.createElement('button');
 
@@ -11,22 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     button.appendChild(buttonText);
 
-    divContainer.appendChild(button);
+    document.body.appendChild(button);
+
+    document.body.appendChild(divContainer);
 
     let classId = '1';
 
     let colorsArry = ['orange', 'purple', 'green', 'red', 'black', 'yellow', 'pink', 'blue',];
 
     function changeColor() {
-           let randomColor = colorsArry[Math.floor(Math.random() * colorsArry.length)];
-           return randomColor;
-        }
+        let randomColor = colorsArry[Math.floor(Math.random() * colorsArry.length)];
+        return randomColor;
+    }
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         console.log("clicked");
 
+
         let div = document.createElement('div');
-        document.body.appendChild(div);
+        divContainer.appendChild(div);
         div.style.backgroundColor = "black";
         div.style.height = "10em";
         div.style.width = "10em";
@@ -39,20 +42,17 @@ document.addEventListener("DOMContentLoaded", function() {
         div.setAttribute('id', classId);
         classId++;
 
-        div.addEventListener('mouseover',function() {
+        div.addEventListener('mouseover', function () {
             let divText = document.createTextNode(div.id);
             div.appendChild(divText);
-            div.style.color = 'white';
-            div.style.textAlign = 'center';
-            div.style.fontSize = '20';
             console.log(div.id);
         });
 
-        div.addEventListener('mouseout', function() {
+        div.addEventListener('mouseout', function () {
             div.innerHTML = "";
         })
 
-        div.addEventListener('click', function() {
+        div.addEventListener('click', function () {
             let randomColor = changeColor();
             div.style.backgroundColor = randomColor;
         })
@@ -60,19 +60,32 @@ document.addEventListener("DOMContentLoaded", function() {
         div.addEventListener('dblclick', removeDivBox);
 
 
-        function removeDivBox () {
-            if(div.id % 2 === 0) {
-                div.nextSibling.remove();
-                    
-            } else { 
+        function removeDivBox() {
+            if (div.id % 2 === 0) {
+                if (div.nextSibling) {
+                    div.nextSibling.remove();
+                } else {
+                    alert('The next box does not exist');
+                }
+
+            }
+            if (div.id % 2 !== 0) {
+                if (div.previousSibling) {
                 div.previousSibling.remove();
+                } else {
+                alert('The previous sibling does not exist');
+                }
             }
             
+
+
+
+
         }
+    })
 
         
-
-    })
+    
     
 
 
